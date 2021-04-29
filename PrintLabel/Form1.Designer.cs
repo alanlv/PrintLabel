a49bf65405ebd774f853f93b6130f643e22e99a3
FYG <token>
2021-04-29 17:26:51 +0800

    添加项目文件。

---
+++ PrintLabel/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace PrintLabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.importcsv = new System.Windows.Forms.Button();
+            this.labelTemplate = new System.Windows.Forms.Button();
+            this.printbutton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.coun = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // importcsv
+            // 
+            this.importcsv.Location = new System.Drawing.Point(47, 55);
+            this.importcsv.Name = "importcsv";
+            this.importcsv.Size = new System.Drawing.Size(75, 23);
+            this.importcsv.TabIndex = 1;
+            this.importcsv.Text = "导入CSV";
+            this.importcsv.UseVisualStyleBackColor = true;
+            this.importcsv.Click += new System.EventHandler(this.importcsv_Click);
+            // 
+            // labelTemplate
+            // 
+            this.labelTemplate.Location = new System.Drawing.Point(47, 102);
+            this.labelTemplate.Name = "labelTemplate";
+            this.labelTemplate.Size = new System.Drawing.Size(75, 23);
+            this.labelTemplate.TabIndex = 1;
+            this.labelTemplate.Text = "选择模板";
+            this.labelTemplate.UseVisualStyleBackColor = true;
+            this.labelTemplate.Click += new System.EventHandler(this.labelTemplate_Click);
+            // 
+            // printbutton
+            // 
+            this.printbutton.Location = new System.Drawing.Point(47, 158);
+            this.printbutton.Name = "printbutton";
+            this.printbutton.Size = new System.Drawing.Size(75, 23);
+            this.printbutton.TabIndex = 1;
+            this.printbutton.Text = "打印";
+            this.printbutton.UseVisualStyleBackColor = true;
+            this.printbutton.Click += new System.EventHandler(this.printbutton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 208);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(524, 315);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(407, 23);
+            this.textBox2.TabIndex = 0;
+            // 
+            // coun
+            // 
+            this.coun.AutoSize = true;
+            this.coun.Location = new System.Drawing.Point(181, 164);
+            this.coun.Name = "coun";
+            this.coun.Size = new System.Drawing.Size(40, 17);
+            this.coun.TabIndex = 3;
+            this.coun.Text = "count";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(183, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(271, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 535);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.printbutton);
+            this.Controls.Add(this.labelTemplate);
+            this.Controls.Add(this.importcsv);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button importcsv;
+        private System.Windows.Forms.Button labelTemplate;
+        private System.Windows.Forms.Button printbutton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label coun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PrintLabel/Form1.cs
@@ -0,0 +1,175 @@
+﻿using LabelManager2;
+using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PrintLabel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void importcsv_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                DataTable dt = new DataTable();
+                string Filepath = System.IO.Path.GetDirectoryName(file.FileName) + "\\" + file.SafeFileName; //獲取目錄信息 + 選擇的文件名和擴展名
+                this.textBox1.Text = Filepath;
+                dt = open_file(Filepath); //把數據寫入dataTable
+                this.dataGridView1.DataSource = dt;  //把寫入的DataTable數據源 賦值給datagridview
+                this.label1.Text = dt.Rows.Count.ToString();
+                this.label2.Text = "0";
+
+            }
+        }
+
+        private static DataTable open_file(string Filepath)   
+        {
+            FileStream fs = new FileStream(Filepath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs, System.Text.Encoding.Default);
+            DataTable dt = new DataTable();
+            string strLine = "";
+            //記錄每行記錄中的各字段內容
+            string[] aryLine = null;
+            string[] tableHead = null;
+            //標示列數
+            int columnCount = 2;
+            //標示是否是讀取的第一行
+            bool IsFirst = true;
+            //逐行讀取CSV中的數據
+            while ((strLine = sr.ReadLine()) != null)
+            {
+                if (IsFirst == true)
+                {
+                    tableHead = strLine.Split(',');//
+                    IsFirst = false;
+                    columnCount = tableHead.Length;
+                    //創建列
+                    for (int i = 0; i < columnCount; i++)
+                    {
+                        tableHead[i] = tableHead[i].Replace("\"", "");
+                        DataColumn dc = new DataColumn(tableHead[i]);
+                        dt.Columns.Add(dc);
+                    }
+                }
+                else
+                {
+                    aryLine = strLine.Split(',');
+                    DataRow dr = dt.NewRow();
+                    for (int j = 0; j < columnCount; j++)
+                    {
+                        dr[j] = aryLine[j].Replace("\"", "");
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            sr.Close();
+            fs.Close();
+            return dt;
+        }
+
+        private void labelTemplate_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                string Filepath = System.IO.Path.GetDirectoryName(file.FileName) + "\\" + file.SafeFileName; //獲取目錄信息 + 選擇的文件名和擴展名
+                this.textBox2.Text = Filepath;
+            }
+        }
+
+        private void printbutton_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text!= "" && textBox2.Text!="")
+            {
+                Printlabel(textBox1.Text, textBox2.Text);
+            }
+            else
+            {
+                MessageBox.Show("请导入csv文件并且选择正确的label 模板");           
+            }
+        }
+
+        /// <summary>
+        /// 遍历csv文件，调用label档实现列印
+        /// </summary>
+        /// <param name="csvpath"> csv文件路径</param> 
+        /// <param name="labelpath"> label图档路径 </param>
+        public  void Printlabel(string csvpath,string labelpath)
+        {
+
+            FileStream fs = new FileStream(csvpath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            //StreamReader sr = new StreamReader(fs, encoding);
+            //string fileContent = sr.ReadToEnd();
+            //記錄每次讀取的一行記錄
+            DataTable dt1 = new DataTable();
+            string strLine = "";
+            //記錄每行記錄中的各字段內容
+            string[] aryLine = null;
+            string[] tableHead = null;
+            //標示列數
+            int columnCount = 2;
+            //標示是否是讀取的第一行
+            bool IsFirst = true;
+            //逐行讀取CSV中的數據
+            while ((strLine = sr.ReadLine()) != null)
+            {
+                if (IsFirst == true)
+                {
+                    tableHead = strLine.Split(',');
+                    IsFirst = false;
+                    columnCount = tableHead.Length;
+                    //創建列
+                    for (int i = 0; i < columnCount; i++)
+                    {
+                        tableHead[i] = tableHead[i].Replace("\"", "");
+                        DataColumn dc1 = new DataColumn(tableHead[i]);
+                        dt1.Columns.Add(dc1);
+                    }
+                }
+                else
+                {
+                    aryLine = strLine.Split(',');
+                    DataRow dr1 = dt1.NewRow();
+                    for (int j = 0; j < columnCount; j++)
+                    {
+                        dr1[j] = aryLine[j].Replace("\"", "");
+                    }
+                    dt1.Rows.Add(dr1);
+
+                    ApplicationClass lbl = new ApplicationClass(); //codesoft7軟件 引用的dll            
+                    lbl.Documents.Open(labelpath, false);// 调用设计好的label文件
+                    Document doc = lbl.ActiveDocument;
+                    //  doc.Variables.FormVariables.Item("Var0").Value = para1; //給差數傳值
+
+                    string temp;
+                    for (int j = 0; j < columnCount; j++)
+                    {
+                        // -----说明---
+                        // label 图档 差数值以ASCII +A  以此累加 参考other文件
+                        // 需要codesoft软件
+                        temp = "ASCII" + Convert.ToChar(65 + j).ToString();
+                        doc.Variables.FormVariables.Item(temp).Value = dr1[j].ToString(); //给参数传值
+                    }
+                    doc.PrintDocument(1);
+
+                    label2.Text = (Convert.ToDouble(label2.Text) +1).ToString();
+                }
+            }
+
+            sr.Close();
+            fs.Close();
+            
+        }
+
+    }
+}
